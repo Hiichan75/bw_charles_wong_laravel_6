a40915cae8a872d94b791f81c2e3ddd87ee454d2
Hiichan75 <kimsojung19950712@gmail.com>
2024-06-11 17:49:03 +0200

    concact seeder fix

--- database/seeders/ContactMessagesTableSeeder.php
+++ database/seeders/ContactMessagesTableSeeder.php
@@ -12,14 +12,14 @@ class ContactMessagesTableSeeder extends Seeder
         ContactForm::insert([
             [
                 'name' => 'John Doe',
-                'email' => 'john.doe@example.com',
+                'email' => '123@123.com',
                 'message' => 'This is a message from John Doe.',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'name' => 'Jane Smith',
-                'email' => 'jane.smith@example.com',
+                'email' => '1234@1234.com',
                 'message' => 'This is a message from Jane Smith.',
                 'created_at' => now(),
                 'updated_at' => now(),
