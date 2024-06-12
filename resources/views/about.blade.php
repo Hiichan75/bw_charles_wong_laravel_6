a145412cee9f1ff5035005643979100dbb5b919f
Hiichan75 <kimsojung19950712@gmail.com>
2024-06-12 18:05:08 +0200

    sources/citations update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,13 +3,14 @@
 @section('content')
 <div class="container mt-5">
         <h1>About This Project</h1>
-        <p>This project was developed as part of a Laravel course assignment. It includes features such as user authentication, profile management, news posting, FAQ management, and a contact form.</p>
+        <p>This project was developed as part of a Laravel course assignment. It includes many features such as user authentication, profile management, news posting, FAQ management, and a contact form, etc...</p>
 
         <h2>Citations and Sources</h2>
         <ul>
             <li><a href="https://laravel.com/docs">Laravel Documentation</a></li>
             <li><a href="https://startbootstrap.com">Start Bootstrap</a></li>
-            <li><a href="https://github.com/Hiichan75/bw_charles_wong_laravel_6">GitHub Repository</a></li>
+            <li><a href="https://github.com/Hiichan75/bw_charles_wong_laravel_6">GitHub Repository/Sources linked</a></li>
+            <li><a href="https://chatgpt.com/?model=gpt-4o">CHAT GPT 4o model</a></li>
         </ul>
     </div>
 @endsection
